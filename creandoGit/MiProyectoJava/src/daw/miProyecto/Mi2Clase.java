babc3de658ac82fd4ca4dd2eb9f673e48e97ec32
Nur <lua_tika@msn.com>
2024-10-21 13:36:50 +0200

    Agregar todos los archivos de nuevo al seguimiento

---
+++ creandoGit/MiProyectoJava/src/daw/miProyecto/Mi2Clase.java
@@ -0,0 +1,10 @@
+package daw.miProyecto; // Asegúrate de que esta línea esté en la parte superior del archivo
+
+public class Mi2Clase {
+    public static void main(String[] args) 
+    {
+        System.out.println("¡Hola, esta es mi clase2!");
+
+        
+    }
+}
---
+++ creandoGit/MiProyectoJava/src/miCommit2.java
@@ -0,0 +1,6 @@
+public class miCommit2 {
+    public static void main(String[] args) {
+        System.out.println("Esta es mi segunda clase con mi segundo commit");
+    }
+    
+}
