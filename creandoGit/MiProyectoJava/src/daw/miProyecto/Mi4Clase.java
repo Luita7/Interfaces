33ae2531a9c58de5a33eeb5d9117e6cdf1068fde
Nur <lua_tika@msn.com>
2024-10-21 18:29:26 +0200

    Nuevo mensaje en mi4clase

--- creandoGit/MiProyectoJava/src/daw/miProyecto/Mi4Clase.java
+++ creandoGit/MiProyectoJava/src/daw/miProyecto/Mi4Clase.java
@@ -1,8 +1,8 @@
 package daw.miProyecto;
 
 public class Mi4Clase {
-    public static void main
-    {
-    
-    }    
+    public static void main(String[] args) {
+        System.out.println("Commit de 4Clase con App y Commit en misma carpeta");
+    }
+        
 }
--- creandoGit/MiProyectoJava/src/daw/miProyecto/miCommit2.java
+++ creandoGit/MiProyectoJava/src/daw/miProyecto/miCommit2.java
@@ -1,3 +1,4 @@
+package daw.miProyecto;
 public class miCommit2 {
     public static void main(String[] args) {
         System.out.println("Esta es mi segunda clase con mi segundo commit");
